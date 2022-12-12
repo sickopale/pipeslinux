7544c4f237f2b2da468e6d5e6054d119d06e6c0f
sickopale <108342539+sickopale@users.noreply.github.com>
2022-12-12 18:32:29 +0300

    Update main.cpp

--- PipesLinux/Main/main.cpp
+++ PipesLinux/Main/main.cpp
@@ -1,36 +1,31 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <iostream>
 
-std::string process_a_path = "../../A/cmake-build-debug/A";
-std::string process_m_path = "../../M/cmake-build-debug/M";
-std::string process_p_path = "../../P/cmake-build-debug/P";
-std::string process_s_path = "../../S/cmake-build-debug/S";
+using namespace std;
 
+int main (int argc, char ** argv) {
+    int i;
 
-int RunProcess(const std::string& process_path) {
-    int pipes [3];
-    pipe(pipes);
-    if (!fork()) {
-        if (dup2(pipes[1], 1) == -1) {
-            return -1;
+    for( i=1; i<argc-1; i++)
+    {   //0 - stdin, 1 - stdout
+        int pd[2]; // pd[0] - end of read, pd[1] - end of write
+        pipe(pd);
+        cout << "launched " << argv[i] << "\n";
+        if (!fork()) {
+            dup2(pd[1], 1);
+            execlp(argv[i], argv[i], NULL);
+            perror("exec");
+            abort();
         }
-        system(process_path.c_str());
-        return 0;
-    } else if (dup2(pipes[0], 0) == -1) {
-        close(pipes[1]);
-        return -1;
-    }
-    return 0;
-}
 
-int main() {
-    if (RunProcess(process_a_path)==0) {
-        if (RunProcess(process_m_path)==0) {
-            if (RunProcess(process_p_path)==0) {
-                system(process_s_path.c_str());
-            }
-        }
+
+        dup2(pd[0], 0); // 0 - stdin
+        close(pd[1]);
     }
 
-    return 0;
+    execlp(argv[i], argv[i], NULL);
+    perror("exec");
+    abort();
 }
